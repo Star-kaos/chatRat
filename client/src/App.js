7c317ff24f1c470648485a41324f11a5591da051
Elijah Perera <elijahperera@Elijahs-MBP.lan>
2022-10-09 22:19:55 -0700

    completed base off app

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,32 @@
 import { io } from 'socket.io-client'
+import { useState } from 'react';
 
 
 function App() {
+  const [message, setMessage] = useState('');
+  const [completeMessage, setCompletedMessage] = useState('');
+
+
   const socket = io('http://localhost:4000')
   socket.on('connect', () => {
     console.log("you connected with", socket.id)
-    socket.emit("hi", 1, "hi")
   })
 
+  function handleMessage(event) {
+    setCompletedMessage(message)
+    socket.emit(completeMessage)
+    event.preventDefault();
+    document.getElementById("messageInput").value = ""
+  }
 
   return (
     <div className="App">
       <h1>chatRat apps</h1>
+      <form onSubmit={handleMessage}>
+        <input id="messageInput" type="text" onChange={event => setMessage(event.target.value)}></input>
+        <input type="Submit"></input>
+      </form>
+      <h1>{completeMessage ? completeMessage : ""}</h1>
     </div>
   );
 }
--- server/server.js
+++ server/server.js
@@ -6,7 +6,7 @@ const io = require("socket.io")(4000, {
 
 io.on("connection", socket => {
     console.log("working", socket.id)
-    socket.on("hi", (number, string) => {
-        console.log(number, string)
+    socket.on("hi", (string) => {
+        console.log(string)
     })
 })
